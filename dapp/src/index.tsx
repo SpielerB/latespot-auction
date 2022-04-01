97f4a02819d793a9bd1fbd3a5ce28f5bcae3d919
SpielerB <32329914+SpielerB@users.noreply.github.com>
2022-04-01 11:30:34 +0200

    Remove web vitals from index Add creation of the react root component Add check for existing react portal targets

--- dapp/src/index.tsx
+++ dapp/src/index.tsx
@@ -2,16 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById('dapp-root')
-);
+const root = document.createElement('div');
+document.body.appendChild(root);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const desktopElement = document.getElementById('dapp-root');
+const mobileElement = document.getElementById('dapp-root-mobile');
+
+const clearChildren = (element: HTMLElement) => {
+    let child;
+    while((child = element.firstChild)) {
+        element.removeChild(child);
+    }
+}
+
+if (desktopElement && mobileElement) {
+    clearChildren(desktopElement);
+    clearChildren(mobileElement);
+
+    ReactDOM.render(
+        <React.StrictMode>
+            <App desktopElement={desktopElement} mobileElement={mobileElement}/>
+        </React.StrictMode>,
+        root);
+} else {
+    console.error('Missing mobile or desktop element (dapp-root and dapp-root-mobile)')
+}
