bb3b823da28b7360adf5bf1fc0f67eec4237d153
SpielerB <32329914+SpielerB@users.noreply.github.com>
2022-09-04 04:32:17 +0200

    Add timeout to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -34,6 +34,7 @@ export default {
         hardhat: {},
         hardhatLocal: {
             url: 'HTTP://127.0.0.1:8545',
+            timeout: 1000000,
             accounts: [
                 '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
                 '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',
