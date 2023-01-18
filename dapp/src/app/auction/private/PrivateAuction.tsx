3608ea6daa6559a70a857a0ea54ded5e761cbce4
J-Saravia <58182196+J-Saravia@users.noreply.github.com>
2023-01-18 15:37:45 +0100

    Finished header design of private auction content.

--- dapp/src/app/auction/private/PrivateAuction.tsx
+++ dapp/src/app/auction/private/PrivateAuction.tsx
@@ -16,28 +16,35 @@ const PrivateAuction = () => {
     const pendingTransaction = usePendingTransaction();
     const contractError = useTransactionError();
 
+    const ticketsSold = contractModel?.privateAuction.ticketsSold ?? 0;
+    const ticketSupply = contractModel?.privateAuction.ticketSupply ?? 0;
+    const soldString = ticketsSold.toLocaleString('de-CH');
+    const supplyString = ticketSupply.toLocaleString('de-CH');
+    const ethereum = ethers.utils.formatEther(BigNumber.from(contractModel?.privateAuction.price));
+
     return (
         <div className="mint-section">
             <div className="mint-c">
                 <div className="mint-header">
-                    <div>
-                        <h4 className="mint-h4">phase 1</h4>
-                        <h1 className="mint-h1">WHITELIST SALE</h1>
-                        {contractModel?.privateAuction.ticketsSold} / {contractModel?.privateAuction.ticketSupply} TICKETS
-                        LEFT <br/>
+                    <h4 className="mint-h4">phase 1</h4>
+                    <h1 className="mint-h1">WHITELIST SALE</h1>
+                    <h3 className="mint-h3">{soldString} / {supplyString} TICKETS LEFT </h3>
+                    <div className="mint-live">
                         <img
                             src="https://assets.website-files.com/621e34ea4b3095856cff1ff8/6226563ba9df1423307642dd_live-icon.svg"
                             loading="lazy"
                             alt=""
                             className="mint-live-icon"
                         />
-                        Ticket Price: 42 usd
-                        = {ethers.utils.formatEther(BigNumber.from(contractModel?.privateAuction.price))} ETH
+                        <div className="mint-live-p">
+                            Ticket Price: 42 USD = {ethereum} ETH
+                        </div>
                     </div>
-                    <div className="upperRight">
-                        <div>Whitelisted: {contractModel?.whitelisted ? "Yes" : "No"}</div>
-                        <button onClick={() => dispatch(buyTickets(2))}>Buy</button>
-                    </div>
+                </div>
+
+                <div className="upperRight">
+                    <div>Whitelisted: {contractModel?.whitelisted ? "Yes" : "No"}</div>
+                    <button onClick={() => dispatch(buyTickets(2))}>Buy</button>
                 </div>
                 <div>Pending Transaction: {pendingTransaction ? "Yes" : "No"}</div>
                 <div>Contract Error: {contractError}</div>
