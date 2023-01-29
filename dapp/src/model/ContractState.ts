dca18f250b0b5a7aacdd3a7e86c74704f293ce43
SpielerB <32329914+SpielerB@users.noreply.github.com>
2023-01-29 23:25:07 +0100

    Update the contract state model for the new token models and the updated transaction handling

--- dapp/src/model/ContractState.ts
+++ dapp/src/model/ContractState.ts
@@ -1,11 +1,22 @@
 import ContractMetadata from './ContractMetadata';
 import Contract from './Contract';
 import Transaction from './Transaction';
+import TokenMetadata from './TokenMetadata';
+import ContractToken from './ContractToken';
 
 export default interface ContractState {
     metadata: ContractMetadata;
     contractModel?: Contract;
+    rawTokens: ContractToken[]
+    tokenMetadata: {
+        [key: number]: TokenMetadata
+    };
     pendingTransactions: {
         [key: string]: Transaction | undefined
     };
+    rawTokensSyncPending: boolean,
+    tokenMetadataSyncPending: {
+        [key: number]: boolean
+    },
+    contractSyncPending: boolean,
 }
