8403b53c5b3f8fab2f946c74436331b7997c1788
SpielerB <32329914+SpielerB@users.noreply.github.com>
2023-02-07 22:57:15 +0100

    Add deploy script for release

--- eth/production/scripts/deploy.ts
+++ eth/production/scripts/deploy.ts
@@ -1,16 +1,65 @@
 import "./helper/productionScript"
-import {network} from 'hardhat';
 import question from './helper/question';
+import {Wallet} from 'ethers';
+import {ensureEnv} from './helper/productionScript';
+import {ethers, run, upgrades} from 'hardhat';
 
 async function main() {
-    if (network.name === "mainnet") {
-        console.warn("You are about to deploy this contract on the Ethereum main network. Write YES if you want to continue");
+    const ownerWallet = new Wallet(process.env.CONTRACT_OWNER_PRIVATE_KEY as string, ethers.provider);
+    const signerWallet = new Wallet(process.env.CONTRACT_SIGNER_PRIVATE_KEY as string, ethers.provider);
+    const parameters = {
+        tokenName: ensureEnv("CONTRACT_PARAMETER_TOKEN_NAME"),
+        tokenSymbol: ensureEnv("CONTRACT_PARAMETER_TOKEN_SYMBOL"),
+        signerAddress: signerWallet.address,
+        baseURI: ensureEnv("CONTRACT_PARAMETER_BASE_URI"),
+        contractMetadataURI: ensureEnv("CONTRACT_PARAMETER_CONTRACT_METADATA_URI"),
+        linkAddress: ensureEnv("CONTRACT_PARAMETER_LINK_ADDRESS"),
+        wrapperAddress: ensureEnv("CONTRACT_PARAMETER_WRAPPER_ADDRESS")
+    }
+    console.warn("You are about to deploy this contract with the following parameters:");
+    console.log(`\towner => ${ownerWallet.address}`);
+    for (const key of Object.keys(parameters)) {
+        console.log(`\t${key} => ${(parameters as any)[key]}`)
+    }
+
+    console.info("Write YES if you want to continue:")
+    {
         const response = await question("> ");
         if (response !== "YES") {
             console.error("Aborting deployment")
             return;
         }
     }
+    console.info("Deploying contract...");
+
+    const factory = await ethers.getContractFactory("AuctionV2Upgradeable", ownerWallet);
+    const contract = await upgrades.deployProxy(factory, [
+        parameters.tokenName,
+        parameters.tokenSymbol,
+        parameters.signerAddress,
+        parameters.baseURI,
+        parameters.contractMetadataURI,
+        parameters.linkAddress,
+        parameters.wrapperAddress
+    ]);
+
+    console.info("Waiting for confirmation...");
+    await contract.deployTransaction.wait(6);
+
+    console.info(`Contract deployed to ${contract.address}.`)
+    console.info("Do you want to verify the contract on etherscan:");
+    {
+        const response = await question("> ");
+        if (response !== "YES") {
+            return;
+        }
+    }
+
+    await run("verify:verify", {
+        address: contract.address,
+        constructorArguments: []
+    })
+
 }
 
 main()
--- eth/production/scripts/helper/productionScript.ts
+++ eth/production/scripts/helper/productionScript.ts
@@ -11,6 +11,12 @@ export const waitFor = async (transactionPromise: Promise<ContractTransaction>) 
     await (await transactionPromise).wait();
 }
 
+export const ensureEnv = (key: string): string => {
+    const value = process.env[key];
+    if (value === undefined) throw `Environment variable '${key}' is not defined`;
+    return value;
+}
+
 let contractInstance: Contract | undefined;
 export const contract = () => {
 
