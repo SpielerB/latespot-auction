929f34edb2f87f32a3377d2416828979d7360142
J-Saravia <58182196+J-Saravia@users.noreply.github.com>
2023-02-08 21:37:05 +0100

    Revised rendering of the tokens and spell-checked staking page.

--- dapp/src/app/staking/Staking.tsx
+++ dapp/src/app/staking/Staking.tsx
@@ -5,30 +5,43 @@ import React, {useCallback} from 'react';
 import {useAddress} from '../../hooks/WalletHooks';
 
 export const Staking = () => {
-    const tokens = useContractTokens();
+    const contractTokens = useContractTokens();
     const contractModel = useContractModel();
     const tokensPending = useRawTokensSyncPending();
     const walletAddress = useAddress();
-    const showTokensLoading = !tokens && tokensPending;
+    const showTokensLoading = !contractTokens && tokensPending;
+
+    const renderTokens = useCallback(() => {
+        if (!showTokensLoading) {
+            let tokens;
+            if (contractTokens.length > 0) {
+                tokens = contractTokens;
+            } else {
+                tokens = [...new Array(4)];
+            }
+            return tokens.map((token, index) => <Token key={`token.${token?.id ?? -index}`} token={token}/>);
+        }
+        return showTokensLoading && <div>Loading tokens...</div>;
+    }, [contractTokens, tokensPending]);
 
     const renderTokensInfo = useCallback(() => {
-        if (tokens) {
+        if (contractTokens) {
             if (contractModel?.tokensRevealed) {
-                if (tokens.length === 0) {
-                    return <div>There are no squirrels in your wallet. Go and get one to start staking.</div>
+                if (contractTokens.length === 0) {
+                    return <div>There are no Squirrels in your wallet. Go and get one to start staking.</div>
                 }
             } else {
                 return (
                     <>
-                        <div>The squirrel degens NFTs have not been revealed.</div>
-                        <div>Once the reveal is over, your NFTs will be stakable via the staking terminal</div>
+                        <div>The Squirrel degens NFTs have not been revealed.</div>
+                        <div>Once the reveal is over, your NFTs will be stakeable via the staking terminal.</div>
                     </>
                 )
             }
         } else {
             return <div>Loading tokens...</div>;
         }
-    }, [tokens, tokensPending, contractModel]);
+    }, [contractTokens, tokensPending, contractModel]);
 
     return (
         <div className="staking">
@@ -37,13 +50,13 @@ export const Staking = () => {
                 <div className="mint-wallet-p"><h3 className="mint-h3">CONNECTED WALLET: </h3>{walletAddress}</div>
             </div>
             <h2 className="staking-title">MY SQUIRRELS</h2>
-            <p>
+            <div>
                 <h3 className="staking-tokens-info">
                     {renderTokensInfo()}
                 </h3>
-            </p>
+            </div>
             <div className="staking-tokens">
-                {tokens && !showTokensLoading && tokens.map(token => <Token key={`token.${token.id}`} token={token}/>)}
+                {renderTokens()}
             </div>
             <div className="staking-info">
                 <h2 className="staking-title">IMPORTANT STAKING INFORMATION</h2>
@@ -56,20 +69,19 @@ export const Staking = () => {
                             <li>Gold Pass (180 days)</li>
                         </ol>
                         <span>
-                        Have a look at the detailed utilities of each level under the "Holder benefits" section on our webpage.
+                        Have a look at the detailed utilities of each level under the "Holder Benefits" section on our webpage.
                     </span>
                     </li>
                     <li>
-                        <h3 className="mint-buy-h4">A squirrel may only be upgraded once!</h3>
-                        <div>Once upgraded, the level and it's utilities will be fixed</div>
-                        <div>
-                            If the first level (Bronze) isn't reached the NFT can unstaked without upgrading and may be
-                            staked again at any time
-                        </div>
+                        <h3 className="mint-buy-h4">A Squirrel may only be upgraded once!</h3>
                         <div>
                             As soon as the first level (Bronze) has been reached, the NFT will automatically be upgraded
-                            if
-                            it gets unstaked.
+                            if it gets unstaked.
+                        </div>
+                        <div>Once upgraded, the level and its utilities will be fixed</div>
+                        <div>
+                            If the first level (Bronze) isn't reached, the NFT can be unstaked without upgrading and may be
+                            staked again at any time.
                         </div>
                     </li>
                     <li>
@@ -85,13 +97,11 @@ export const Staking = () => {
                         <h3 className="mint-buy-h4">Staking and unstaking may take a while</h3>
                         <div>
                             The process of staking or unstaking the NFT may take a while, depending on the current
-                            network
-                            congestion and your selected gas price.
+                            network congestion and your selected gas price.
                         </div>
                         <div>
-                            All information about ongoing transactions such as staking or unstaking are lost upon
-                            reloading
-                            the page.
+                            All information about ongoing transactions, such as staking or unstaking, is lost upon
+                            reloading the page.
                         </div>
                         <div>
                             Therefore, if you initiated a transaction to stake or unstake a token, please wait up to 5
