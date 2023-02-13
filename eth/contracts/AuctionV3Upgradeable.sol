d31e9b7e11a067773707b6f772107df2b607b9df
SpielerB <32329914+SpielerB@users.noreply.github.com>
2023-02-13 20:27:56 +0100

    Add royalties and operator filter to the Auction V3 contract

--- eth/contracts/AuctionV3Upgradeable.sol
+++ eth/contracts/AuctionV3Upgradeable.sol
@@ -1,17 +1,13 @@
 /// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts-upgradeable/utils/math/MathUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/StringsUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "./@chainlink/VRFV2WrapperConsumerBaseUpgradeable.sol";
+import "operator-filter-registry/src/upgradeable/DefaultOperatorFiltererUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721RoyaltyUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/common/ERC2981Upgradeable.sol";
+import "./AuctionV3Base.sol";
 
 /// @title Upgradeable 2 Phase mint for the squirrel degens NFT project (V3)
-contract AuctionV3Upgradeable is ERC721Upgradeable, OwnableUpgradeable, VRFV2WrapperConsumerBaseUpgradeable {
+contract AuctionV3Upgradeable is AuctionV3Base {
     using MathUpgradeable for uint;
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
@@ -20,117 +16,10 @@ contract AuctionV3Upgradeable is ERC721Upgradeable, OwnableUpgradeable, VRFV2Wra
 
     using StringsUpgradeable for uint256;
 
-    /*
-        Constants
-    */
-    uint32 constant callbackGasLimit = 100000;
-    uint16 constant requestConfirmations = 3;
-    uint32 constant numWords = 1;
-
-    /*
-        Initialisation
-    */
-    CountersUpgradeable.Counter private _tokenCounter; // Used to track new and existing (from V2) tokens
-    uint256 public preMintCount;
-
-    address public signatureAddress;
-
-    string private __baseURI;
-    string private __realURI;
-    string private _contractURI;
-
-    mapping(address => bool) private _whitelistMap;
-
-    /*
-        Staking
-    */
-    event TokenStaked(address indexed _owner, uint256 indexed _token);
-    event TokenUnStaked(address indexed _owner, uint256 indexed _token);
-
-    uint256[] private _definedStakeLevels;
-    mapping(uint256 => address) private _stakeOwnerMap;
-    mapping(uint256 => uint256) private _stakeLevelTimeMap;
-    mapping(uint256 => uint256) private _stakeStartTimeMap;
-
-    /*
-        Auction
-    */
-    address[] private _ticketHolders;
-
     event PrivateMintStarted(uint256 _price, uint256 _supply, uint256 _tokensPerWallet);
-
-    bool public privateAuctionStarted;
-    bool public privateAuctionStopped;
-    uint256 public privateAuctionPrice;
-    uint256 public privateAuctionTicketCount;
-    uint256 public privateAuctionTicketSupply;
-    uint256 public privateAuctionTicketsPerWallet;
-    mapping(address => uint256) private privateAuctionTicketMap;
-
     event PublicMintStarted(uint256 _price, uint256 _supply, uint256 _tokensPerWallet);
-
-    bool private publicAuctionStarted;
-    bool private publicAuctionStopped;
-    uint256 private publicAuctionPrice;
-    uint256 private publicAuctionTicketCount;
-    uint256 private publicAuctionTicketSupply;
-    uint256 private publicAuctionTicketsPerWallet;
-    mapping(address => uint256) private publicAuctionTicketMap;
-
-    /*
-        MintV2
-    */
-    uint256 private _holderIndex;
-    uint256 private _nextHolderTokenIndex;
-
-    /*
-        Reveal
-    */
-
     event Revealed(string _uri, uint256 _seed);
 
-    uint256 public revealVrfRequestId;
-    bool public revealed;
-    uint256 public seed;
-
-    function initialize(string memory tokenName_, string memory tokenSymbol_, address signatureAddress_, string memory baseURI_, string memory contractURI_, address vrfLink_, address vrfWrapper_) public initializer {
-        __ERC721_init(tokenName_, tokenSymbol_);
-        __Ownable_init();
-        __VRFV2WrapperConsumerBase_init(vrfLink_, vrfWrapper_);
-
-        signatureAddress = signatureAddress_;
-        __baseURI = baseURI_;
-        _contractURI = contractURI_;
-    }
-
-    /*
-       Private Mint
-    */
-    bool public privateMintStarted;
-    bool public privateMintStopped;
-    uint256 public privateMintPrice;
-    uint256 public privateMintSupply;
-    uint256 public privateMintTokensPerWallet;
-    mapping(address => uint256) public privateMintMap;
-    CountersUpgradeable.Counter private _privateMintTokenCounter;
-
-    /*
-       Public Mint
-    */
-    bool public publicMintStarted;
-    bool public publicMintStopped;
-    uint256 public publicMintPrice;
-    uint256 public publicMintSupply;
-    uint256 public publicMintTokensPerWallet;
-    mapping(address => uint256) public publicMintMap;
-    CountersUpgradeable.Counter private _publicMintTokenCounter;
-
-    /*
-    * Returns the contract URI for the contract level metadata
-    */
-    function contractURI() public view returns (string memory) {
-        return _contractURI;
-    }
 
     /*
     * The total supply minted
@@ -140,33 +29,10 @@ contract AuctionV3Upgradeable is ERC721Upgradeable, OwnableUpgradeable, VRFV2Wra
     }
 
     /*
-    * Adds the given addresses to the whitelist for the private mint
-    */
-    function whitelist(address[] memory addresses) public onlyOwner {
-        for (uint256 i = 0; i < addresses.length; ++i) {
-            _whitelistMap[addresses[i]] = true;
-        }
-    }
-
-    /*
-    * Returns a boolean indicating whether the sender wallet is whitelisted
-    */
-    function whitelisted() public view returns (bool) {
-        return _whitelistMap[_msgSender()];
-    }
-
-    /*
     * Returns a boolean indicating whether the sender wallet is whitelisted
     */
     function whitelistedWallet(address wallet) public view onlyOwner returns (bool) {
         return _whitelistMap[wallet];
-    }
-
-    /*
-    * Returns the amount of tickets bought by the sender wallet in the private auction of the V2 contract
-    */
-    function privateAuctionTickets() public view returns (uint256) {
-        return privateAuctionTicketMap[_msgSender()];
     }
 
     /*
@@ -200,37 +66,41 @@ contract AuctionV3Upgradeable is ERC721Upgradeable, OwnableUpgradeable, VRFV2Wra
     * Mint tokens to wallet the private mint is active
     * Only whitelisted addresses may use this method
     */
-    function privateMint() public payable {
-        _privateMint(_msgSender());
+    function privateMint(uint256 count_) public payable {
+        _privateMint(_msgSender(), count_);
     }
-
 
     /*
     * Mint tokens to wallet the private mint is active
     * Only whitelisted addresses may use this method
     */
-    function _privateMint(address wallet) private {
+    function _privateMint(address wallet_, uint256 count_) private {
         // Basic check
         require(privateMintActive(), "Private mint is not active");
 
+        // Count check
+        require(count_ > 0, "At least 1 token has to be minted");
+
         // Whitelist check
-        require(_whitelistMap[wallet], "Wallet is not whitelisted");
+        require(_whitelistMap[wallet_], "Wallet is not whitelisted");
 
         // Value check
         require(msg.value > 0, "Value has to be greater than 0");
         require(msg.value % privateMintPrice == 0, "Value has to be a multiple of the price");
 
         uint256 tokensToMint = msg.value / privateMintPrice;
-        uint256 currentTokens = privateMintMap[wallet];
+        require(tokensToMint == count_, "Given count does not match provided value");
+
+        uint256 currentTokens = privateMintMap[wallet_];
 
         // Token amount check
         require(tokensToMint + currentTokens <= privateMintTokensPerWallet, "Total token count is higher than the max allowed tokens per wallet for the private mint");
         require(_privateMintTokenCounter.current() + tokensToMint <= privateMintSupply, "There are not enough tokens left in the private mint");
 
-        privateMintMap[wallet] += tokensToMint;
+        privateMintMap[wallet_] += tokensToMint;
 
         for (uint256 i = 0; i < tokensToMint; ++i) {
-            _mint(wallet, _tokenCounter.current() + 1);
+            _safeMint(wallet_, _tokenCounter.current() + 1);
             _tokenCounter.increment();
             _privateMintTokenCounter.increment();
         }
@@ -293,32 +163,37 @@ contract AuctionV3Upgradeable is ERC721Upgradeable, OwnableUpgradeable, VRFV2Wra
     /*
     * Mint tokens while the public mint is active
     */
-    function publicMint() public payable {
-        _publicMint(_msgSender());
+    function publicMint(uint256 count_) public payable {
+        _publicMint(_msgSender(), count_);
     }
 
     /*
     * Mint tokens to wallet while the public mint is active
     */
-    function _publicMint(address wallet) private {
+    function _publicMint(address wallet_, uint256 count_) private {
         // Basic check
         require(publicMintActive(), "Public mint is not active");
+
+        // Count check
+        require(count_ > 0, "At least 1 token has to be minted");
 
         // Value check
         require(msg.value > 0, "Value has to be greater than 0");
         require(msg.value % publicMintPrice == 0, "Value has to be a multiple of the price");
 
         uint256 tokensToMint = msg.value / publicMintPrice;
-        uint256 currentTokens = publicMintMap[wallet];
+        require(tokensToMint == count_, "Given count does not match provided value");
+
+        uint256 currentTokens = publicMintMap[wallet_];
 
         // Token amount check
         require(tokensToMint + currentTokens <= publicMintTokensPerWallet, "Total token count is higher than the max allowed tokens per wallet for the public mint");
         require(_publicMintTokenCounter.current() + tokensToMint <= publicMintSupply, "There are not enough tokens left in the public mint");
 
-        publicMintMap[wallet] += tokensToMint;
+        publicMintMap[wallet_] += tokensToMint;
 
         for (uint256 i = 0; i < tokensToMint; ++i) {
-            _mint(wallet, _tokenCounter.current() + 1);
+            _safeMint(wallet_, _tokenCounter.current() + 1);
             _tokenCounter.increment();
             _publicMintTokenCounter.increment();
         }
@@ -357,11 +232,11 @@ contract AuctionV3Upgradeable is ERC721Upgradeable, OwnableUpgradeable, VRFV2Wra
     /*
     * Mint a token for a different wallet, used by crossmint
     */
-    function mintFor(address wallet) public payable {
+    function crossmint(address wallet_, uint256 count_) public payable onlyCrossmint {
         if (privateMintActive()) {
-            _privateMint(wallet);
+            _privateMint(wallet_, count_);
         } else if (publicMintActive()) {
-            _publicMint(wallet);
+            _publicMint(wallet_, count_);
         } else {
             revert("No mint active");
         }
@@ -453,6 +328,25 @@ contract AuctionV3Upgradeable is ERC721Upgradeable, OwnableUpgradeable, VRFV2Wra
     }
 
     /*
+    * Returns a boolean indicating if the token is currently staked
+    */
+    function tokensOf(address wallet) public view returns (uint256[] memory) {
+        uint256[] memory possibleOwnedTokens = new uint256[](totalSupply());
+        uint256 index = 0;
+        for (uint256 i = 1; i <= totalSupply(); ++i) {
+            if (ownerOf(i) == wallet) {
+                possibleOwnedTokens[index++] = i;
+            }
+        }
+        // Copy token ids to correct sized array
+        uint256[] memory ownedTokens = new uint256[](index);
+        for (uint256 i = 0; i < index; ++i) {
+            ownedTokens[i] = possibleOwnedTokens[i];
+        }
+        return ownedTokens;
+    }
+
+    /*
        Return the metadata URI of the given token
     */
     function tokenURI(uint256 tokenId) public view override(ERC721Upgradeable) returns (string memory){
@@ -460,13 +354,6 @@ contract AuctionV3Upgradeable is ERC721Upgradeable, OwnableUpgradeable, VRFV2Wra
         uint256 offset = seed % totalSupply();
         uint256 metaId = ((tokenId + offset) % totalSupply()) + 1;
         return string.concat(__realURI, '/', metaId.toString(), '.json');
-    }
-
-    /*
-       Compatibility functions
-    */
-    function _burn(uint256) internal pure override(ERC721Upgradeable) {
-        revert("Burning is not allowed");
     }
 
 }
--- eth/production/scripts/upgrade.ts
+++ eth/production/scripts/upgrade.ts
@@ -7,11 +7,24 @@ import {confirmation} from './helper/question';
 async function main() {
     const proxyAddress = ensureEnv("UPGRADE_PROXY_ADDRESS");
     const owner = getOwner();
-    await confirmation("You are about to upgrade an existing AuctionV2Upgradeable contract to V3.");
+    const crossmintWallet = ensureEnv("UPGRADE_CONTRACT_PARAMETER_CROSSMINT_WALLET");
+    const royaltiesWallet = ensureEnv("UPGRADE_CONTRACT_PARAMETER_ROYALTIES_WALLET");
+    await confirmation(
+        "You are about to upgrade an existing AuctionV2Upgradeable contract to V3 with the following parameters:",
+        `\tproxyAddress => ${proxyAddress}`,
+        `\tcrossmintWallet => ${crossmintWallet}`,
+        `\troyaltiesWallet => ${royaltiesWallet}`,
+    );
 
     const factory = await ethers.getContractFactory("AuctionV3Upgradeable", owner);
     console.info("Upgrading contract...")
     const upgradedProxyContract = await upgrades.upgradeProxy(proxyAddress, factory);
+
+    console.info("Initializing AuctionV3Upgradeable...");
+    await upgradedProxyContract.initializeV3(crossmintWallet, royaltiesWallet);
+
+    console.info("Waiting for confirmation...")
+    await upgradedProxyContract.deployTransaction.wait(6);
 
     console.info("Contract has been upgraded to V3.")
 
