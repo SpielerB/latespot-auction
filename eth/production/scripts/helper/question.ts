8322598b588878e06a4e0e312f99de22dd21d9ca
SpielerB <32329914+SpielerB@users.noreply.github.com>
2023-02-13 20:32:03 +0100

    Allow for multiple messages in the confirmation helper

--- eth/production/scripts/helper/question.ts
+++ eth/production/scripts/helper/question.ts
@@ -7,8 +7,10 @@ const rl = createInterface({
 
 export const question = (questionText: string) => new Promise<string>(resolve => rl.question(questionText, resolve));
 
-export const confirmation = async (message: string) => {
-    console.info(message);
+export const confirmation = async (...params: string[]) => {
+    for (let i = 0; i < params.length; i++) {
+        console.info(params[i]);
+    }
     console.info("Write 'YES' if you want to continue:");
     const response = await question("> ");
     if (response !== "YES") throw "Confirmation rejected by user.";
